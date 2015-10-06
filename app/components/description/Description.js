fd70fb3135d1f477ff25d0713aa6be7afc8c9b59
miteymouse <chenghtmark@gmail.com>
2015-10-06 10:42:15 +0800

    adds react-parallax and image file

--- app/components/description/Description.js
+++ app/components/description/Description.js
@@ -3,11 +3,14 @@ import DescriptionOverview from './DescriptionOverview';
 import Students from './Students';
 import Teachers from './Teachers';
 import Parents from './Parents';
+import Parallax from 'react-parallax';
 
 class Description extends React.Component{
   render(){
     return(
       <div>
+        <Parallax bgImage="img/parallaxOne.png" strength={500}>
+        </Parallax>
         <DescriptionOverview />
         <Students />
         <Teachers />
