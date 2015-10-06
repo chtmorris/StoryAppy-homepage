bb0707bfcaba678fdb838485ed192e8eb0d9988c
chtmorris <agoosester@gmail.com>
2015-10-06 18:30:47 +0800

    added full page login screen

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Footer extends React.Component{
   render(){
     return(
-        <div className="footer">
-          <p className="footer-text"> &copy; 2015 StoryAppy </p>
-        </div>
+      <div className="footer">
+        <p className="footer-text"> &copy; 2015 StoryAppy </p>
+      </div>
     )
   }
 }
