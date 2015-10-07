71641787cd61f2e5c1de48884a339c4b246d90e6
chtmorris <agoosester@gmail.com>
2015-10-07 18:00:19 +0800

    added react DB for email form

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import { Parse } from 'parse';
+import secrets from '../config/secrets';
+
+Parse.initialize(secrets.parse.applicationId, secrets.parse.javascriptKey);
 
 class Main extends React.Component{
   render() {
--- app/components/intro/Intro.js
+++ app/components/intro/Intro.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import Parse from 'parse';
+
+let contact = new Parse.Object('Contact');
+
+function validateEmail(email){
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
 
 class Intro extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      emailSent: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event) {
+    event.preventDefault();
+    var emailAddress = React.findDOMNode(this.refs.emailInput).value
+    if ((emailAddress != "") && validateEmail(emailAddress)) {
+      contact.set('email', emailAddress);
+      contact.save();
+      this.setState({emailSent: true});
+    } else {
+      console.log("empty email field");
+    }
+  }
   render(){
+    let form;
+    if (!this.state.emailSent) {
+      form = (
+        <form>
+          <div className="grid-block">
+            <div className="medium-8 grid-block">
+              <input ref="emailInput" type="text" placeholder="Email Address" className="e-mail" required />
+            </div>
+            <div className="medium-4 grid-block">
+              <a onClick={this.handleClick} className="expand button submit-email" href="#">Submit</a>
+            </div>
+          </div>
+        </form>
+      );
+    } else {
+      form = (
+        <div className="vertical grid-block">
+          <div className="grid-content">
+            <p className="get-email-text">
+              Thanks for getting in touch. We will get back to you asap. 
+            </p>
+          </div>
+        </div>
+      );
+    }
     return(
       <div className="align-right vertical grid-block intro-block shadow">
         <div className="intro-content">
@@ -17,16 +68,7 @@ class Intro extends React.Component{
                   If you’re interested in a free trial of StoryAppy, please sign up here. </p>
               </div>
               <div className="grid-block align-center">
-                <form>
-                  <div className="grid-block">
-                    <div className="medium-8 grid-block">
-              			  <input type="text" placeholder="Email Addresss" className="e-mail"/>
-                    </div>
-                    <div className="medium-4 grid-block">
-              			  <a className="expand button submit-email" href="#">Submit</a>
-                    </div>
-                  </div>
-                </form>
+                {form}
               </div>
             </div>
           </div>
