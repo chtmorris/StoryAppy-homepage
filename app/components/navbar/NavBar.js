b1dc76a7dabf2ed385817957189deb692061a886
chtmorris <agoosester@gmail.com>
2015-10-09 13:58:25 +0800

    fixed navbar hide

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -9,13 +9,13 @@ class NavBar extends React.Component{
 
       <div className="primary title-bar">
         <div className="grid-block">
-          <div className="medium-11 grid-block">
+          <div className="medium-11 grid-block initial-positioning">
           <span className="left">
             <img src="img/StoryAppyLogoSmall.png" className="logo" />
             <span className="logo-text">StoryAppy</span>
           </span>
           </div>
-          <div className="medium-1 grid-block nav-links">
+          <div className="medium-1 grid-block nav-links initial-positioning">
             <span className="right login-button">
               <Link to="login-section" spy={true} smooth={true} duration={2000}>
                 Sign up
